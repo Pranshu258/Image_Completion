b82e9e1e5893422637df0f47501e343b4ff6942c
Pranshu Gupta <prangupt@microsoft.com>
2018-11-17 01:29:51 +0530

    moved values to config

---
+++ config.py
@@ -0,0 +1,7 @@
+SRC_FOLDER = "images/source/"
+OUT_FOLDER = "images/output/"
+
+PRINT_BB_IMAGE = True
+
+IMAGE = ""
+BB_IMAGE_SUFFIX = "_BB.png"
--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 """
-STATISTICS OF PATCH OFFSETS FOR IMAGE COMPLETION - KAIMING HE & JIAN SUN
+Statistics of Patch Offsets for Image Completion - Kaiming He and Jian Sun
+A Python Implementation - Pranshu Gupta and Shrija Mishra
 """
 
 import cv2
+import sys
 import numpy as np
-
-SRC_FOLDER = "images/source/"
-OUT_FOLDER = "images/output/"
-IMAGE = "7796"
+import config as cfg
 
 def get_bounding_box(mask):
+    """Get Bounding Box for a Binary Mask"""
     a = np.where(mask != 0)
     bbox = np.min(a[0]), np.max(a[0]), np.min(a[1]), np.max(a[1])
+    if cfg.PRINT_BB_IMAGE:
+        cv2.rectangle(mask, (bbox[2], bbox[0]), (bbox[3], bbox[1]), (0,0,255), 1)
+        cv2.imwrite(cfg.OUT_FOLDER + cfg.IMAGE + cfg.BB_IMAGE_SUFFIX, mask)
     return bbox
 
-def main (imageFile, maskFile):
+def main(imageFile, maskFile):
+    """
+    Image Completion Pipeline - Image and Mask 
+    """
     image = cv2.imread(imageFile)
     mask = cv2.imread(maskFile)
     bb = get_bounding_box(mask)
-    print bb
 
 if __name__ == "__main__":
-    imageFile = SRC_FOLDER + IMAGE + ".jpg"
-    maskFile = SRC_FOLDER + IMAGE + ".png"
+    if len(sys.argv) != 3:
+        print "Usage: python main.py image_name mask_file_name"
+        exit()
+    cfg.IMAGE = sys.argv[1].split('.')[0]
+    imageFile = cfg.SRC_FOLDER + sys.argv[1]
+    maskFile = cfg.SRC_FOLDER + sys.argv[2]
     main(imageFile, maskFile)
     
